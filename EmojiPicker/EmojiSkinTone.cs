05aab9a5a2775cf2efbbe3238d9a6d4392f8f450
Tomas Bezouska <tb@ampcap.com>
2018-10-10 15:20:37 +0200

    Picker dialog

---
+++ EmojiPicker/EmojiSkinTone.cs
@@ -0,0 +1,14 @@
+﻿namespace EmojiPicker
+{
+    public class EmojiSkinTone
+    {
+        public EmojiSkinTone(string emoji, string name)
+        {
+            this.Emoji = emoji;
+            this.Name = name;
+        }
+
+        public string Emoji { get; }
+        public string Name { get; }
+    }
+}
